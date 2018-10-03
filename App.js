3b496420c0bbd8bb1104e45faa9d419889ab6f98
Fredrik Jenssen <fredjen.contactor@gmail.com>
2018-10-03 14:39:19 +0200

    Reverted App.js changes to avoid merge conflicts

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import BottomNav from './BottomNav'
-import {Text, View} from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
     render() {
         return (
-            <View style={{
-                flex: 1,
-            }}>
-                <View style={{
-                    flex: 1,
-                    height: 2000,
-                    width: 400,
-                    backgroundColor: 'blue',
-                    justifyContent: 'center'
-                }}>
-                    <Text style={{fontSize: 30}}>heihei</Text>
-                </View>
-                <BottomNav/>
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Text>What a memer!</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
