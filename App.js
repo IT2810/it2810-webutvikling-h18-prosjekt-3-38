8435a5f159da1a852833255699602f9c7ba00620
Fredrik Jenssen <fredjen.contactor@gmail.com>
2018-10-03 14:25:00 +0200

    #2 Implemented component for bottom navigation bar

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import BottomNav from './BottomNav'
+import {Text, View} from "react-native";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>What a memer!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+            }}>
+                <View style={{
+                    flex: 1,
+                    height: 2000,
+                    width: 400,
+                    backgroundColor: 'blue',
+                    justifyContent: 'center'
+                }}>
+                    <Text style={{fontSize: 30}}>heihei</Text>
+                </View>
+                <BottomNav/>
+            </View>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
