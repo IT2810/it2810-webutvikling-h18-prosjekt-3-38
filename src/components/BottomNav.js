d708f1e93f2d86eafa26356ad0d2db94fa5f926d
Fredrik Jenssen <fredjen.contactor@gmail.com>
2018-10-04 11:40:33 +0200

    #13 Changes background color of BottomNav based on active tab

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,28 +1,31 @@
 import React, {Component} from "react";
 import BottomNavigation from "react-native-material-ui/src/BottomNavigation/BottomNavigation.react";
-import { Entypo } from '@expo/vector-icons/';
+import {Entypo} from '@expo/vector-icons/';
 
 // Implements Material-UI's bottom navigation component
 class BottomNav extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { active: 'home' };
+        this.state = {
+          active: 'home',
+          backgroundColor: '#fa5457'
+        };
     }
     render() {
         return (
-            <BottomNavigation active={this.state.active}>
+            <BottomNavigation style={{ container: {backgroundColor: this.state.backgroundColor} }} active={this.state.active}>
                 <BottomNavigation.Action
                     key="home"
                     icon="home"
                     label="Home"
-                    onPress={() => this.setState({ active: 'home' })}
+                    onPress={() => this.setState({ active: 'home', backgroundColor: '#fa5457' })}
                 />
                 <BottomNavigation.Action
                     key="graph"
                     icon={<Entypo name="line-graph" size={25}/>}
                     label="Graph"
-                    onPress={() => this.setState({ active: 'graph' })}
+                    onPress={() => this.setState({ active: 'graph', backgroundColor: '#01b4bc' })}
                 />
             </BottomNavigation>
         );
