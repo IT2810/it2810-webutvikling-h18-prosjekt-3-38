b1e28d10ad4ece5477c5a016b6b96ef742d98749
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2018-10-10 12:48:47 +0200

    Tried to fix test

--- __tests__/src/components/NavigatorTest.js
+++ __tests__/src/components/NavigatorTest.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Navigator from '../../../src/components/Navigator'
-import { JestSerializer as test } from 'enzyme-to-json'
+import FrontDisplayFlexBox from '../../../src/components/FrontDisplayFlexBox'
 
-test('Rendered correctly', () => {
-  const tree = renderer.create(<Navigator/>).toJSON()
-  expect(tree).toMatchSnapshot()
+// test('Rendered correctly', () => {
+//  const tree = renderer.create(<Navigator />).toJSON()
+//  expect(tree).toMatchSnapshot()
+// })
+
+let NavigatorComponent = renderer.create(<Navigator />).getInstance()
+beforeAll(() => {
+  NavigatorComponent.navigate('Home')
+})
+it('Expect screen to be home', () => {
+  expect(NavigatorComponent.children).toBe(<FrontDisplayFlexBox />)
 })
