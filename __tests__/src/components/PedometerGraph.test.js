2b12e073897da2bd1c21502f8655f967d3bd186b
skykanin <3789764+skykanin@users.noreply.github.com>
2018-10-14 14:16:24 +0200

    added commented out faulty test for doc reference

--- __tests__/src/components/PedometerGraph.test.js
+++ __tests__/src/components/PedometerGraph.test.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import PedometerGraph from '../../../src/components/PedometerGraph'
+import { AsyncStorage } from 'react-native'
 
 /*
-jest.useFakeTimers() does not correctly mock javascript Date() timers.
+  The jest.useFakeTimers() function does not correctly mock javascript Date() timers.
+  Which is why you will get a warning from jest when running this test, even though
+  the test cases all work as expected.
 */
 describe('PedometerGraph', () => {
   const component = renderer.create(<PedometerGraph />)
@@ -15,7 +18,24 @@ describe('PedometerGraph', () => {
 
   test('retrieves data correctley', () => {
     component.getInstance().getData().then(() => {
-      expect(component.getInstance().state.stepData.length).not.toBeLessThan(1)
+      return expect(component.getInstance().state.stepData.length).not.toBeLessThan(1)
     })
   })
+
+  // Test not working as expected
+  /* test('gets data correctly from async storage', () => {
+    const data = [{ x: 'mon', y: 2340 }, { x: 'tus', y: 2057 }]
+    return expect(
+      AsyncStorage.setItem('motivationData', JSON.stringify(data))
+        .catch((err) => Promise.reject(console.log('Error occurred:', err)))
+        .then(() => AsyncStorage.getAllKeys())
+        .catch((err) => Promise.reject(console.log('Error occurred: ', err)))
+        /* .then(() => AsyncStorage.getItem('motivationData'))
+        .catch((err) => Promise.reject(console.log('Error occurred:', err)))
+    ).resolves.toEqual([])
+  }) */
 })
+
+/* return component.getInstance()._retrieveMotivationData().then(() => {
+  expect(component.getInstance().state.motivationData).toEqual(data)
+}) */
